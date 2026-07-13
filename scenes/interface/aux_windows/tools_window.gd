93dc21641843a5a997f291b334bb3323a18f7fda
erlog135 <22063718+erlog135@users.noreply.github.com>
2026-07-13 12:35:24 -0400

    improve ui

--- scenes/interface/aux_windows/tools_window.gd
+++ scenes/interface/aux_windows/tools_window.gd
@@ -2,9 +2,26 @@ extends Control
 
 @onready var tool_list: ItemList = $ToolList
 
+const TOOL_CIRCLE = preload("uid://buco5y6muxvhn")
+const TOOL_LINE_PEN = preload("uid://c1epqd3vwb1b5")
+const TOOL_PAN = preload("uid://mh3kx6yvter7")
+const TOOL_RECTANGLE = preload("uid://dy4p0t40vnyy2")
+const TOOL_SELECT = preload("uid://0g8p7lvto7v8")
+const TOOL_TRANSFORM = preload("uid://b0w4xtlid8vne")
+
+
+const TOOL_ICONS: Dictionary[EditorState.Tool,Resource] = {
+	EditorState.Tool.SELECT: TOOL_SELECT,
+	EditorState.Tool.TRANSFORM: TOOL_TRANSFORM,
+	EditorState.Tool.LINE_PEN: TOOL_LINE_PEN,
+	EditorState.Tool.CIRCLE: TOOL_CIRCLE,
+	EditorState.Tool.RECTANGLE: TOOL_RECTANGLE,
+	EditorState.Tool.PAN: TOOL_PAN,
+}
+
 func _ready() -> void:
 	for tool in EditorState.Tool.keys():
-		tool_list.add_item(tool.capitalize())
+		tool_list.add_item(tool.capitalize(),TOOL_ICONS[EditorState.Tool[tool]])
 
 	tool_list.item_selected.connect(_on_tool_selected)
 	EditorState.tool_changed.connect(_on_editor_tool_changed)
